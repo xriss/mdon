ae9c484704229433a857f3f826fdaec552617dbd
kriss <kriss@xixs.com>
2023-04-26 13:01:26 +0100

    test

---
+++ code/js/cmd.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+/*
+
+(C) Kriss@XIXs.com 2023 and released under the MIT license.
+
+See https://github.com/xriss/mdon for full notice.
+
+*/
+
+const cmd=exports;
+
+const pfs=require("fs").promises
+
+
+const ls=function(a) { console.log(util.inspect(a,{depth:null})); }
+
+
+cmd.parse=function(argv)
+{
+	argv.filename_cmd=__filename
+
+}
+
+
+cmd.run=async function(argv)
+{
+	if( argv._[0]=="to" )
+	{
+		await require("./to.js").test(argv)
+		return
+	}
+
+	// help text
+	console.log(
+`
+>	mdon to 
+
+Test code.
+
+
+`)
+}
+
+// if global.argv is set then we are inside another command so do nothing
+if(!global.argv)
+{
+	var argv = require('yargs').argv
+	global.argv=argv
+	cmd.parse(argv)
+	cmd.run(argv)
+}
